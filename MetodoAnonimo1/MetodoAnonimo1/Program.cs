e5b214d94045438509977813778022c4648b9dcf
Ángela Mora Mata <amormat1010@cpifpalanturing.es>
2025-10-27 14:08:46 +0100

    añadir ejercicios de metodos anonimos

---
+++ MetodoAnonimo1/MetodoAnonimo1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MetodoAnonimo1
+{
+    public class Program
+    {
+        public delegate bool ValidarEdades(int x);
+
+        static void Main(string[] args)
+        {
+            ValidarEdades validate = delegate (int edad)
+            {
+                return edad > 18;
+            };
+
+            Console.WriteLine( validate(20)); 
+            Console.WriteLine(validate(15));
+        }
+    }
+}
---
+++ MetodoAnonimo2/MetodoAnonimo2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MetodoAnonimo1
+{
+    public class Program
+    {
+        public delegate bool ValidarEdades(int x);
+
+        static void ImprimirConsola(ValidarEdades validar, int edad)
+        {
+            Console.WriteLine(validar(edad));
+        }
+
+        static void Main(string[] args)
+        {
+            ValidarEdades validate = delegate (int edad)
+            {
+                return edad > 18;
+            };
+            ImprimirConsola(validate, 19);
+            ImprimirConsola(validate, 20);
+            ImprimirConsola(validate, 15);
+        }
+    }
+}
